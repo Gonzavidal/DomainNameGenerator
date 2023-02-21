4acb7d735daf50a29cf9a7671f1de253779d674a
Gonzavidal <gvidal1992@gmail.com>
2023-02-21 15:56:42 +0000

    Avance final

--- src/app.js
+++ src/app.js
@@ -5,7 +5,22 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector(".domain").innerHTML = generateDomain();
+};
+
+let generateDomain = () => {
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let tld = [".com", ".net", ".us", ".es"];
+
+  let pronounIndex = Math.floor(Math.random() * pronoun.length);
+  let adjIndex = Math.floor(Math.random() * adj.length);
+  let nounIndex = Math.floor(Math.random() * noun.length);
+  let tldIndex = Math.floor(Math.random() * tld.length);
+
+  return (
+    pronoun[pronounIndex] + adj[adjIndex] + noun[nounIndex] + tld[tldIndex]
+  );
 };
